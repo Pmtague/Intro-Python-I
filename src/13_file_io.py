974533cf77dbe6b7c440f3d90402eed5a49a68f8
Penny Tague <pmtague9@gmail.com>
2020-01-09 11:08:41 -0800

    Completed 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,11 +8,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+with open('/Users/pmtag/Documents/Intro-Python-I/src/foo.txt') as f:
+	print(f.read())
+
+print(f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('/Users/pmtag/Documents/Intro-Python-I/src/bar.txt', "w+") as f:
+	f.write("I don't like goats\n Just kidding, I love goats\n Alright, fine, goats are my only friends")
